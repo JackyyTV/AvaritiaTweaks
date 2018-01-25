74d17e5db7be06d623d57246eb05ef4fa3cf415c
Jacky Scrubby <jacky1356400@gmail.com>
2018-01-25 21:42:25 +0800

    Fixed indents

--- src/main/java/jackyy/avaritiatweaks/AvaritiaTweaks.java
+++ src/main/java/jackyy/avaritiatweaks/AvaritiaTweaks.java
@@ -13,7 +13,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = AvaritiaTweaks.MODID, version = AvaritiaTweaks.VERSION, name = AvaritiaTweaks.MODNAME, dependencies = AvaritiaTweaks.DEPENDS, acceptedMinecraftVersions = AvaritiaTweaks.MCVERSION, guiFactory = AvaritiaTweaks.GUIFACTORY, certificateFingerprint = "@FINGERPRINT@", useMetadata = true)
 public class AvaritiaTweaks {
 
-	public static final String MODID = "avaritiatweaks";
+    public static final String MODID = "avaritiatweaks";
     public static final String MODNAME = "Avaritia Tweaks";
     public static final String VERSION = "1.10.2-1.0";
     public static final String MCVERSION = "[1.10,1.11)";
@@ -24,23 +24,23 @@ public class AvaritiaTweaks {
 
     public static Logger logger = LogManager.getLogger(MODNAME);
 
-	@SidedProxy(serverSide = AvaritiaTweaks.COMMON_PROXY, clientSide = AvaritiaTweaks.CLIENT_PROXY)
-	public static CommonProxy proxy;
+    @SidedProxy(serverSide = AvaritiaTweaks.COMMON_PROXY, clientSide = AvaritiaTweaks.CLIENT_PROXY)
+    public static CommonProxy proxy;
 
-	@Mod.EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-		proxy.preInit(event);
-	}
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        proxy.preInit(event);
+    }
 
-	@Mod.EventHandler
-	public void init(FMLInitializationEvent event) {
-		proxy.init(event);
-	}
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        proxy.init(event);
+    }
 
-	@Mod.EventHandler
-	public void postInit(FMLPostInitializationEvent event) {
-		proxy.postInit(event);
-	}
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
+    }
 
     @Mod.EventHandler
     public void onFingerprintViolation(FMLFingerprintViolationEvent event) {
